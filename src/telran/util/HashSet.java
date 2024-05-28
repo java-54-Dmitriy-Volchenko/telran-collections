1370266a96a3fa1edda117c9821f6cd9d39ae565
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-05-28 20:20:39 +0300

    intermediate result, all methods written - but sizeTest not working

--- src/telran/util/HashSet.java
+++ src/telran/util/HashSet.java
@@ -1,7 +1,7 @@
 package telran.util;
 
 import java.util.Iterator;
-
+import java.util.NoSuchElementException;
 
 @SuppressWarnings("unchecked")
 
@@ -11,21 +11,38 @@ public class HashSet<T> implements Set<T> {
 	List<T> [] hashTable;
 	int size;
 	float factor;
-	private class HashSetIterator implements Iterator<T> {
-
-		@Override
-		public boolean hasNext() {
-			// TODO Auto-generated method stub
-			return false;
+	
+		private class HashSetIterator implements Iterator<T> {
+		    int currentIndex = 0;
+		    Iterator<T> currentIterator = null;
+		   
+	
+		    @Override
+		    public boolean hasNext() {
+		    
+	
+		        while (currentIndex < hashTable.length && (currentIterator==null||!currentIterator.hasNext())) {
+		            List<T>list=hashTable[currentIndex++];
+		            if(list!=null) {
+		            	currentIterator=list.iterator();
+		            }
+		        }
+	
+		        return currentIterator!=null && currentIterator.hasNext();
+		    }
+	
+		   
+	
+		    @Override
+		    public T next() {
+		        if (!hasNext()) {
+		            throw new NoSuchElementException();
+		        }
+		        return currentIterator.next();
+		    }
 		}
 
-		@Override
-		public T next() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-	}
+
 	public HashSet(int hashTableLength, float factor) {
 		hashTable = new List[hashTableLength];
 		this.factor = factor;
@@ -44,13 +61,11 @@ public class HashSet<T> implements Set<T> {
 			size++;
 			res = true;
 		}
-		
-		
 		return res;
 	}
 
 	private void hashTableReallocation() {
-		List<T>[]tmp = new List[hashTable.length * 2];
+		List<T>[] tmp = new List[hashTable.length * 2];
 		for(List<T> list: hashTable) {
 			if(list != null) {
 				for(T obj: list) {
@@ -59,27 +74,34 @@ public class HashSet<T> implements Set<T> {
 			}
 		}
 		hashTable = tmp;
-		
 	}
-	private void addObjInHashTable(T obj, List<T> [] table) {
+
+	private void addObjInHashTable(T obj, List<T>[] table) {
 		int index = getIndex(obj);
 		List<T> list = table[index];
-		if(list == null) {
+		if (list == null) {
 			list = new LinkedList<>();
-			hashTable[index] = list;
+			table[index] = list;
 		}
 		list.add(obj);
-		
 	}
+
 	private int getIndex(T obj) {
 		int hashCode = obj.hashCode();
 		int index = Math.abs(hashCode % hashTable.length);
 		return index;
 	}
+
 	@Override
 	public boolean remove(T pattern) {
-		// TODO Auto-generated method stub
-		return false;
+		int index = getIndex(pattern);
+		boolean res = false;
+		List<T> list = hashTable[index];
+		if (list != null && list.remove(pattern)) {
+			size--;
+			res = true;
+		}
+		return res;
 	}
 
 	@Override
@@ -91,20 +113,25 @@ public class HashSet<T> implements Set<T> {
 
 	@Override
 	public int size() {
-		
 		return size;
 	}
 
 	@Override
 	public Iterator<T> iterator() {
-		
 		return new HashSetIterator();
 	}
 
 	@Override
 	public T get(T pattern) {
-		// TODO Auto-generated method stub
+		int index = getIndex(pattern);
+		List<T> list = hashTable[index];
+		if (list != null) {
+			for (T element : list) {
+				if (element.equals(pattern)) {
+					return element;
+				}
+			}
+		}
 		return null;
 	}
-
 }
