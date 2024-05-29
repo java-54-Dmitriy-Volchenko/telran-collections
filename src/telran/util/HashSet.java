3ab5a19ef0e964c8cf013ed8295511f3dbe22dea
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-05-29 19:54:21 +0300

    improve class HashSet to avoid multiply returns and inplements constructor to HashSetIterator to initiate moving to a next element

--- src/telran/util/HashSet.java
+++ src/telran/util/HashSet.java
@@ -4,134 +4,152 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 @SuppressWarnings("unchecked")
-
 public class HashSet<T> implements Set<T> {
-	private static final int DEFAULT_HASH_TABLE_LENGTH = 16;
-	private static final float DEFAULT_FACTOR = 0.75f;
-	List<T> [] hashTable;
-	int size;
-	float factor;
-	
-		private class HashSetIterator implements Iterator<T> {
-		    int currentIndex = 0;
-		    Iterator<T> currentIterator = null;
-		   
-	
-		    @Override
-		    public boolean hasNext() {
-		    
-	
-		        while (currentIndex < hashTable.length && (currentIterator==null||!currentIterator.hasNext())) {
-		            List<T>list=hashTable[currentIndex++];
-		            if(list!=null) {
-		            	currentIterator=list.iterator();
-		            }
-		        }
-	
-		        return currentIterator!=null && currentIterator.hasNext();
-		    }
-	
-		   
-	
-		    @Override
-		    public T next() {
-		        if (!hasNext()) {
-		            throw new NoSuchElementException();
-		        }
-		        return currentIterator.next();
-		    }
-		}
+    private static final int DEFAULT_HASH_TABLE_LENGTH = 16;
+    private static final float DEFAULT_FACTOR = 0.75f;
+    List<T>[] hashTable;
+    int size;
+    float factor;
+
+    public HashSet(int hashTableLength, float factor) {
+        hashTable = new List[hashTableLength];
+        this.factor = factor;
+    }
+
+    public HashSet() {
+        this(DEFAULT_HASH_TABLE_LENGTH, DEFAULT_FACTOR);
+    }
+
+    private class HashSetIterator implements Iterator<T> {
+        int currentIndex = 0;
+        Iterator<T> currentIterator = null;
+        T nextElement = null;
+
+        public HashSetIterator() {
+            moveToNextValidIterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextElement != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T res = nextElement;
+            moveToNextValidIterator();
+            return res;
+        }
+
+        private void moveToNextValidIterator() {
+            nextElement = null;
+            while (currentIndex < hashTable.length) {
+                if (currentIterator == null || !currentIterator.hasNext()) {
+                    List<T> basket = hashTable[currentIndex++];
+                    if (basket != null) {
+                        currentIterator = basket.iterator();
+                    }
+                }
+                if (currentIterator != null && currentIterator.hasNext()) {
+                    nextElement = currentIterator.next();
+                    return;
+                }
+            }
+        }}
+   
+    @Override
+    public boolean add(T obj) {
+        boolean res = false;
+        if (!contains(obj)) {
+            if ((float) size / hashTable.length >= factor) {
+                hashTableReallocation();
+            }
+            addObjInHashTable(obj, hashTable);
+            size++;
+            res = true;
+        }
+        return res;
+    }
+
+    private void hashTableReallocation() {
+        List<T>[] tmp = new List[hashTable.length * 2];
+        for (List<T> list : hashTable) {
+            if (list != null) {
+                for (T obj : list) {
+                    addObjInHashTable(obj, tmp);
+                }
+            }
+        }
+        hashTable = tmp;
+    }
+
+    private void addObjInHashTable(T obj, List<T>[] table) {
+        int index = getIndex(obj);
+        List<T> list = table[index];
+        if (list == null) {
+            list = new LinkedList<>();
+            table[index] = list;
+        }
+        list.add(obj);
+    }
+
+    private int getIndex(T obj) {
+        int hashCode = obj.hashCode();
+        int index = Math.abs(hashCode % hashTable.length);
+        return index;
+    }
+
+    @Override
+    public boolean remove(T pattern) {
+        int index = getIndex(pattern);
+        List<T> basket = hashTable[index];
+        boolean res = false;
+        if (basket != null && basket.remove(pattern)) {
+            size--;
+            if (basket.size() == 0) {
+                hashTable[index] = null;
+            }
+            res = true;
+        }
+        return res;
+    }
+
+    @Override
+    public boolean contains(T pattern) {
+        int index = getIndex(pattern);
+        List<T> list = hashTable[index];
+        return list != null && list.contains(pattern);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new HashSetIterator();
+    }
+
+    @Override
+    public T get(T pattern) {
+        int index = getIndex(pattern);
+        List<T> basket = hashTable[index];
+        T res = null;
+        
+        if (basket != null) {
+            for (T element : basket) {
+                if (element.equals(pattern)) {
+                    res = element;
+                }
+            }
+        }
+        return res;
+    }
+
+    }
 
 
-	public HashSet(int hashTableLength, float factor) {
-		hashTable = new List[hashTableLength];
-		this.factor = factor;
-	}
-	public HashSet() {
-		this(DEFAULT_HASH_TABLE_LENGTH, DEFAULT_FACTOR);
-	}
-	@Override
-	public boolean add(T obj) {
-		boolean res = false;
-		if(!contains(obj)) {
-			if((float)size / hashTable.length >= factor) {
-				hashTableReallocation();
-			}
-			addObjInHashTable(obj, hashTable);
-			size++;
-			res = true;
-		}
-		return res;
-	}
-
-	private void hashTableReallocation() {
-		List<T>[] tmp = new List[hashTable.length * 2];
-		for(List<T> list: hashTable) {
-			if(list != null) {
-				for(T obj: list) {
-					addObjInHashTable(obj, tmp);
-				}
-			}
-		}
-		hashTable = tmp;
-	}
-
-	private void addObjInHashTable(T obj, List<T>[] table) {
-		int index = getIndex(obj);
-		List<T> list = table[index];
-		if (list == null) {
-			list = new LinkedList<>();
-			table[index] = list;
-		}
-		list.add(obj);
-	}
-
-	private int getIndex(T obj) {
-		int hashCode = obj.hashCode();
-		int index = Math.abs(hashCode % hashTable.length);
-		return index;
-	}
-
-	@Override
-	public boolean remove(T pattern) {
-		int index = getIndex(pattern);
-		boolean res = false;
-		List<T> list = hashTable[index];
-		if (list != null && list.remove(pattern)) {
-			size--;
-			res = true;
-		}
-		return res;
-	}
-
-	@Override
-	public boolean contains(T pattern) {
-		int index = getIndex(pattern);
-		List<T> list = hashTable[index];
-		return list != null && list.contains(pattern);
-	}
-
-	@Override
-	public int size() {
-		return size;
-	}
-
-	@Override
-	public Iterator<T> iterator() {
-		return new HashSetIterator();
-	}
-
-	@Override
-	public T get(T pattern) {
-		int index = getIndex(pattern);
-		List<T> list = hashTable[index];
-		if (list != null) {
-			for (T element : list) {
-				if (element.equals(pattern)) {
-					return element;
-				}
-			}
-		}
-		return null;
-	}
-}
